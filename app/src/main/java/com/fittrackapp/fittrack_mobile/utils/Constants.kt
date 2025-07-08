529960e603d1f0c78644b114afb7e0e719ce5083
Neon <tpshine1234@gmail.com>
2025-07-09 02:07:54 +0630

    update: switch to cloud api

--- app/src/main/java/com/fittrackapp/fittrack_mobile/utils/Constants.kt
+++ app/src/main/java/com/fittrackapp/fittrack_mobile/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.fittrackapp.fittrack_mobile.utils
 
 object Constants {
-    const val BASE_URL = "http://10.0.2.2:8000/api"
+    const val BASE_URL = "https://fittrack-server-bxdw.onrender.com/api"
 }
